1ba72195087fb2e559f2f7c2e941f20890dcbbed
Ricardo <ricardo.chipana@telemovilperu.com>
2018-10-10 19:34:15 -0500

    ggg

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,13 +13,15 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .extract(['jquery','popper.js'])
-    mix.copy('resources/sass/demo.css', 'public/css')
-    mix.copy('resources/sass/themify-icons.css', 'public/css')
-    mix.copy('resources/sass/ie7.css', 'public/css')
-    mix.copy('resources/sass/normalize.css', 'public/css')
-    mix.copy('resources/sass/weather-icons.css', 'public/css')
-    mix.copy('resources/sass/fullcalendar.css', 'public/css')
-    mix.copy('resources/fonts/fonts/fonts', 'public/fonts')
+    .styles([
+        'resources/sass/demo.css',
+        'resources/sass/themify-icons.css',
+        'resources/sass/ie7.css',
+        'resources/sass/normalize.css',
+        'resources/sass/weather-icons.css',
+        'resources/sass/fullcalendar.css',
+    ],'public/css/all.css')
+    .copy('resources/fonts/fonts/fonts','public/fonts')
     .sass('resources/sass/bootstrap/bootstrap.scss', 'public/css')
     .sass('resources/sass/style.scss', 'public/css')
     .sass('resources/sass/socials.scss', 'public/css')
