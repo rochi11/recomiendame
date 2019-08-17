4fb9ba3f3d3f2c3db6ead19057c24a9919e09576
Rommel <ricardo.chipana@telemovilperu.com>
2019-08-17 00:40:31 -0500

    Importar items desde excel

--- database/migrations/2019_03_02_084420_create_products_table.php
+++ database/migrations/2019_03_02_084420_create_products_table.php
@@ -13,19 +13,28 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->increments('idProduct');
-            $table->string('nombre')->nullable();
-            $table->string('marca')->nullable();
-            $table->string('precio')->nullable();
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('idProducto');
+            $table->string('nombreProducto')->nullable();
+            $table->string('marcaProducto')->nullable();
+            $table->string('descripcionProducto')->nullable();
+            $table->string('codigoBarras')->nullable();
+            $table->string('imagenReferencia')->nullable();
+            $table->string('pesoProducto')->nullable();
+            $table->string('ubicacion')->nullable();
+            $table->string('proveedor')->nullable();
+            $table->string('categoria')->nullable();
+            $table->string('origen')->nullable();
+            $table->string('fechaElaboracion')->nullable();
             $table->string('fechaVencimiento')->nullable();
-            $table->string('lote')->nullable();
-            $table->string('peso')->nullable();
-            $table->string('cantidad')->nullable();
-            $table->string('unidadMedida')->nullable();
-            $table->string('descuento')->nullable();
-            $table->string('fechaDescuento')->nullable();
-            $table->string('nroGondola')->nullable();
+            $table->string('cantidadxUnidad')->nullable();
+            $table->string('precioCoste')->nullable();
+            $table->string('precioUnidad')->nullable();
+            $table->string('numeroLote')->nullable();
+            $table->string('unidadesExitentes')->nullable();
+            $table->string('unidadesPedidas')->nullable();
+            $table->string('nivelNuevoPedido')->nullable();
+            $table->string('Suspendido')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
